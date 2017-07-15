59e8e0542904b6eeebc6c4f1e3a13500cfe40740
Mohamed Abo Bdwey <m.bdwey@gmail.com>
2017-07-15 14:09:50 +0200

    Allow auto height Update sharp version

--- lambda/index.js
+++ lambda/index.js
@@ -11,29 +11,37 @@ const URL = process.env.URL;
 
 exports.handler = function(event, context, callback) {
   const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
+  const match = key.match(/(\d+)x(\d+|auto)\/(.*)/);
   const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
+  if (match[2] == 'auto')
+  {
+    var height = null;
+  }
+  else
+  {
+    var height = parseInt(match[2], 10);
+  }
   const originalKey = match[3];
 
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
-    .catch(err => callback(err))
+  .then(data => Sharp(data.Body)
+    .resize(width,height)
+    .toFormat('png')
+    .toBuffer()
+  )
+  .then(buffer => S3.putObject({
+      Body: buffer,
+      Bucket: BUCKET,
+      ContentType: 'image/png',
+      Key: key,
+    }).promise()
+  )
+  .then(() => callback(null, {
+      statusCode: '301',
+      headers: {'location': `${URL}/${key}`},
+      body: '',
+    })
+  )
+  .catch(err => callback(err));
+
 }
